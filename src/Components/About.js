4bf3940f8b803238b0276192428c552388eed97b
alba g <jarazethalba@gmail.com>
2022-06-23 12:04:14 -0500

    .

--- src/Components/About.js
+++ src/Components/About.js
@@ -9,7 +9,7 @@ export default function About() {
           <p className="about-info">
             I'm a computer systems engineering student graduating in 2023,
             passionate about building front end apps using React JS, and React
-            Native. I constantly continue expanding my knowlege by learning new
+            Native. I constantly continue to expand my knowlege by learning new
             technologies and collaborating with other developers.{" "}
           </p>
         </div>
